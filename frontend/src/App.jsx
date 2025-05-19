3c331ad71784fb5c02c0f65d1c1d1728f8044779
Juan Cruz <juancruzvenier@gmail.com>
2025-05-19 11:46:01 -0300

    Puse clasName de Tailwind de ejemplo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,8 @@ function App() {
 
   return (
     <>
-      <div>
+       {/* className="--codigo tailwind--" */}
+      <div className='flex justify-center'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
